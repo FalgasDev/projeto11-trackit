af11e22cdc496491233eef2e44a0519f6174b573
FabioGarroteDev <fgarrote12@gmail.com>
2022-12-16 13:08:07 -0300

    Layout da tela de hoje e da tela de hábitos prontas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import AuthProvider from './AuthContext';
 import TodayPage from './pages/TodayPage';
+import HabitsPage from './pages/HabitsPage';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 					<Route path="/" element={<LoginPage />} />
 					<Route path="/cadastro" element={<RegisterPage />} />
 					<Route path='/hoje' element={<TodayPage />}/>
+					<Route path='/habitos' element={<HabitsPage />}/>
 				</Routes>
 			</BrowserRouter>
 		</AuthProvider>
